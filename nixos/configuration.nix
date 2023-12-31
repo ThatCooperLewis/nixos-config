8e5383d1ba8f868c399d24a02f3239e22108cc09
Cooper Lewis <thatcooperlewis@gmail.com>
2023-12-30 22:04:52 -0800

    Initial nixos commit

---
+++ nixos/configuration.nix
@@ -0,0 +1,245 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      # All docker containers
+	  ./plex-stack.nix
+
+	  # Home Manager
+      # https://nix-community.github.io/home-manager/index.xhtml#sec-install-nixos-module
+      # See unpure-commands.md for initial setup
+      <home-manager/nixos>
+    ];
+
+
+
+  ### Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Expose RAID array
+  boot.swraid.enable = true;
+  boot.swraid.mdadmConf = "MAILADDR=steve@apple.com"; # silence warning during install
+
+  # Define linux kernel
+  # Vanilla NixOS 23.11 had an older kernel that caused some Hyprland/Nvidia issues
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  
+  
+  ### Networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "lewis-linux";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+ 
+
+
+  ### Locale
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+
+  ### Display Configuration
+
+  # The following is a nice backup if Hyprland shits the bed
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  # Enable interop between app UI + screensharing
+  xdg.portal.enable = true;
+  # Not necessary to include hytland portal if hyprland.nvidiaSettings is on
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+
+
+  ### Nvidia, oh Nvidia
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  # Add nvidia to kernel module manually
+  boot.initrd.kernelModules = [ "nvidia" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  # Other configs
+  hardware.nvidia = {
+    # Define driver version
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # Modesetting is required.
+    modesetting.enable = true;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+    # Use the NVidia open source kernel module
+    # Known to be buggy
+    open = true;
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+  };
+
+
+  ### Hyprland
+  services.xserver.displayManager.sddm.enable = true; #This line enables sddm
+  services.xserver.enable = true; # Might need this for Xwayland  
+  programs.hyprland.enable = true;
+  environment.sessionVariables = {
+    # Fixes invisible mouse
+    # This gave me many issues with Hyprland immediately crashing (but running again if I called `Hyprland`)
+    # A mix of other settings here + in home.nix ended up resolving it
+    # I think the main culprit was an outdate linux kernel (see above) plus adding the DRM_DEVICES flag below
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Define which GPU to use (3080)
+    WLR_DRM_DEVICES = "/dev/dri/card0";
+    # Tells electron apps to use Wayland
+    NIXOS_OZONE_WL = "1";
+  };
+  
+
+  
+  ### Audio
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+
+
+  ### Users
+  users = {
+  	defaultUserShell = pkgs.zsh;
+  	users.cooper = {
+  	  useDefaultShell = true;
+      isNormalUser = true;
+      description = "Cooper Lewis";
+      extraGroups = [ "networkmanager" "wheel" "docker" ];
+    };
+  };
+
+
+
+  ### Home Manager
+  # Install packages to /etc/profile instead of ~/.nix-profile – Per the docs, this may become the default in the future
+  home-manager.useUserPackages = true;
+  # Import everything else directly from file
+  home-manager.users.cooper = import ../../../home/cooper/.config/home-manager/home.nix;
+
+
+
+  ### Packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [ "electron-25.9.0" ]; # Obsidian uses EOL electron :( 
+  environment.systemPackages = with pkgs; [
+
+    # Dev Tools
+	btop
+	git
+	kitty
+    micro
+    openssh
+	vscode
+    wget
+    zplug
+    zsh
+
+	# Homelab
+    home-manager
+
+    # Display & Graphics
+    dunst				# Notifications
+    libnotify
+    grim 				# Screenshots
+    networkmanagerapplet
+    rofi-wayland 		# App Switcher
+    swww 				# Wallpaper
+    waybar 				# Toolbar
+	(waybar.overrideAttrs (oldAttrs: {
+		mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+	  })
+	)
+	
+	# Media
+	discord
+	firefox
+    spotify
+    steam
+    tidal-hifi
+	obsidian
+  ];
+
+
+  ### Fonts
+  fonts.packages = with pkgs; [
+    meslo-lgs-nf
+  ];
+
+
+
+  ### File Browsing
+  # programs.thunar.enable = true;
+  # services.gvfs.enable = true; # Mount, trash, other stuff
+  # services.tumbler.enable = true; # Thumbnail images
+  # TODO: Configure thunar more (details in wiki)
+
+
+
+  ### Make server reachable via SSH
+  services.openssh = {
+  	enable = true;
+  	settings = {
+  	  PasswordAuthentication = true;
+  	};
+  };
+
+
+
+  ### Miscellaneous
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # If setting default user shell to zsh, this is necessary
+  programs.zsh.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?  
+}
---
+++ nixos/plex-stack.nix
@@ -0,0 +1,334 @@
+{ config, pkgs, ... }:
+
+# {
+let
+
+  hostIP = "10.0.50.1";
+
+  dataDir = "/mnt/plex-content";
+  dataFallbackDir = "/mnt/plex-content-fallback";
+  data4kDir = "/mnt/plex-content-4k";
+
+  usenetDownloads = "/mnt/plex-downloads/data/usenet";
+  usenet4kDownloads = "/mnt/plex-downloads/data-4k/usenet";
+
+  tdarrTranscodeDir = "/mnt/nas-containers/tdarr/temp";
+  
+  arrConfigDir = "/mnt/config-array";
+  plexConfigDir = "/mnt/config-array/plex/config";
+  plexTranscodeDir = "/mnt/config-array/plex/transcode";
+
+  ports = {
+  	bazarr = 6767;
+  	nzbget = 6789; # 6789 for HTTP, 6791 for HTTPS
+  	nzbgetSSL = 6791;
+  	overseerr = 5055;
+  	plex = 32400;
+  	plexUDP = 32469;
+  	prowlarr = 9696;
+  	radarr = 7878;
+  	radarr4k = 7879;
+  	requestrr = 4545;
+  	sonarr = 8989;
+  	sonarr4k = 8990;
+  	tdarrServer = 8266; # 8265 for Web Portal, 8266 for Node/Server interop
+  	tdarrWeb = 8265;
+  };
+
+  dockerPorts = {
+  	bazarr = ["${toString ports.bazarr}:${toString ports.bazarr}"];
+ 	nzbget = [
+ 	  "${toString ports.nzbget}:${toString ports.nzbget}"
+ 	  "${toString ports.nzbgetSSL}:${toString ports.nzbgetSSL}"
+ 	];
+ 	overseerr = ["${toString ports.overseerr}:${toString ports.overseerr}"];
+    plex = [
+      "${toString ports.plex}:${toString ports.plex}"
+      "${toString ports.plexUDP}:${toString ports.plexUDP}"
+    ];
+    prowlarr = ["${toString ports.prowlarr}:${toString ports.prowlarr}"];
+    radarr = ["${toString ports.radarr}:${toString ports.radarr}"];
+    radarr4k = ["${toString ports.radarr4k}:${toString ports.radarr}"];
+    requestrr = ["${toString ports.requestrr}:${toString ports.requestrr}"];
+    sonarr = ["${toString ports.sonarr}:${toString ports.sonarr}"];
+    sonarr4k = ["${toString ports.sonarr4k}:${toString ports.sonarr}"];
+    tdarr = [ "${toString ports.tdarrWeb}:${toString ports.tdarrWeb}" "${toString ports.tdarrServer}:${toString ports.tdarrServer}" ];
+ };
+  
+in {
+
+  # Configure firewall to allow containers access to each other and external internet
+  config.networking.firewall = {
+    enable = true;
+    # Containers don't get their ports exposed by default
+    allowedTCPPorts = [ ports.bazarr ports.nzbget ports.nzbgetSSL ports.overseerr 
+      					ports.plex ports.prowlarr ports.radarr ports.radarr4k ports.requestrr 
+      					ports.sonarr ports.sonarr4k ports.tdarrServer ports.tdarrWeb 
+    				];
+    allowedUDPPorts = [ ports.plexUDP ];
+    interfaces.docker1 = {
+      # Allow ports to query each other
+      allowedUDPPorts = [ 53 ];
+    };
+  };
+
+  # Install docker, make it compatible with Nvidia GPUs
+  config.virtualisation.docker = {
+  	enable = true;
+  	enableNvidia = true;
+  	enableOnBoot = true;
+  	rootless = {
+  	  enable = true;
+  	  setSocketVariable = true;	
+  	};
+  };
+
+  # libnvidia-container does not support cgroups v2 (prior to 1.8.0)
+  # https://github.com/NVIDIA/nvidia-docker/issues/1447
+  config.systemd.enableUnifiedCgroupHierarchy = false;
+
+  # OCI Backend is Podman by default
+  # It does not support Nvidia GPUs with Plex 
+  config.virtualisation.oci-containers.backend = "docker";
+
+  # From: https://madison-technologies.com/take-your-nixos-container-config-and-shove-it/
+  # Create a shared network so containers can talk to each other
+  config.systemd.services.create-plex-network = with config.virtualisation.oci-containers; {
+    serviceConfig.Type = "oneshot";
+    wantedBy = [ 
+      "${backend}-plex.service" 
+      "${backend}-sonarr.service"
+      "${backend}-sonarr4k.service" 
+      "${backend}-radarr.service" 
+      "${backend}-radarr4k.service" 
+      "${backend}-prowlarr.service" 
+      "${backend}-nzbget.service" 
+      "${backend}-overseerr.service" 
+    ];
+    script = ''${pkgs.docker}/bin/docker network create --driver bridge plex-stack || true'';
+  };
+
+  config.virtualisation.oci-containers.containers = {
+
+    ###########################
+    #### Plex Media Server ####
+    ###########################
+
+    plex = {
+      image = "plexinc/pms-docker:plexpass";
+      ports = dockerPorts.plex;
+      environment = {
+      	PLEX_UID = "950";
+      	PLEX_GID = "950";
+      	PLEX_CLAIM = "claim-s8wyAfhFGKpdEU2JmusY";
+      	ADVERTISE_IP = "http://${hostIP}:${toString ports.plex}";
+      	NVIDIA_VISIBLE_DEVICES = "1";
+      	NVIDIA_DRIVER_CAPABILITIES = "compute,video,utility";
+      };
+      volumes = [
+        "${plexConfigDir}:/config"
+        "${plexTranscodeDir}:/transcode"
+        "${dataDir}:/data"
+        "${dataFallbackDir}:/data-fallback"
+        "${plexTranscodeDir}:/data-4k"
+      ];
+      extraOptions = [
+      	"--runtime=nvidia"
+      	"--gpus=\"device=1\""
+      	"--network=plex-stack"
+      ];
+    };
+
+    #########################
+    #### Sonarr & Radarr ####
+    #########################
+
+    sonarr = {
+      image = "ghcr.io/hotio/sonarr";
+      ports = dockerPorts.sonarr;
+      environmentFiles = [ ./plexDefault.env ];
+      volumes = [
+	    "${arrConfigDir}/sonarr/config:/config"
+ 	    "${dataDir}/shows:/data/shows"
+ 	    "${dataFallbackDir}/shows:/data/shows-fallback"
+ 	    "${dataDir}/anime:/data/anime"
+ 	    "${usenetDownloads}:/data/usenet"
+      ];
+      extraOptions = [ "--network=plex-stack" ];
+    };
+
+    sonarr4k = {
+      image = "ghcr.io/hotio/sonarr";
+      ports = dockerPorts.sonarr4k;
+      environmentFiles = [ ./plexDefault.env ];
+      volumes = [
+	    "${arrConfigDir}/sonarr-4k/config:/config"
+ 	    "${data4kDir}/shows:/data-4k/shows"
+ 	    "${data4kDir}/anime:/data-4k/anime"
+ 	    "${usenet4kDownloads}:/data-4k/usenet"
+      ];
+      extraOptions = [ "--network=plex-stack" ];
+    };
+
+    radarr = {
+      image = "ghcr.io/hotio/radarr";
+      ports = dockerPorts.radarr;
+      environmentFiles = [ ./plexDefault.env ];
+      volumes = [
+	    "${arrConfigDir}/radarr/config:/config"
+ 	    "${dataDir}/movies:/data/movies"
+ 	    "${dataFallbackDir}/movies:/data/movies-fallback"
+ 	    "${usenetDownloads}:/data/usenet"
+      ];    	
+      extraOptions = [ "--network=plex-stack" ];
+    };
+
+    radarr4k = {
+      image = "ghcr.io/hotio/radarr";
+      ports = dockerPorts.radarr4k;
+      environmentFiles = [ ./plexDefault.env ];
+      volumes = [
+	    "${arrConfigDir}/radarr-4k/config:/config"
+ 	    "${data4kDir}/movies:/data-4k/movies"
+ 	    "${usenet4kDownloads}:/data-4k/usenet"
+      ];    	
+      extraOptions = [ "--network=plex-stack" ];
+    };
+
+    ###########################
+    #### Prowlarr & NZBGet ####
+    ###########################
+
+    prowlarr = {
+      image = "ghcr.io/hotio/prowlarr";
+      ports = dockerPorts.prowlarr;
+      environmentFiles = [ ./plexDefault.env ];
+      volumes = [
+        "${arrConfigDir}/prowlarr/config:/config"        
+      ];
+      extraOptions = [ "--network=plex-stack" ];
+    };
+
+    nzbget = {
+      image = "lscr.io/linuxserver/nzbget:latest";
+      ports = dockerPorts.nzbget;
+      environmentFiles = [ ./plexDefault.env ];
+      volumes = [
+        "${arrConfigDir}/nzbget/config:/config"
+ 	    "${usenetDownloads}:/data/usenet"
+ 	    "${usenet4kDownloads}:/data-4k/usenet"
+		# Path to SSL certs used to be provided, but they're dead now       
+      ];
+      extraOptions = [ "--network=plex-stack" ];
+    };
+
+    ###############################
+    #### Overseerr & Requestrr ####
+    ###############################
+
+	overseerr = {
+	  image = "ghcr.io/hotio/overseerr";
+	  ports = dockerPorts.overseerr;
+	  environmentFiles = [ ./plexDefault.env ];
+	  volumes = [
+	    "${arrConfigDir}/overseerr/config:/config"
+	  	"${dataDir}:/data"
+	  	"${dataFallbackDir}:/data-fallback"
+	  	"${data4kDir}:/data-4k"
+	  ];
+      extraOptions = [ "--network=plex-stack" ];
+	};
+
+	requestrr = {
+	  image = "darkalfx/requestrr";
+	  ports = dockerPorts.requestrr;
+	  volumes = [
+	    "${arrConfigDir}/requestrr/config:/config"
+	  ];
+	};
+
+    ########################
+    #### Tdarr & Bazarr ####
+    ########################
+
+    tdarr = {
+      image = "ghcr.io/haveagitgat/tdarr:latest";
+      ports = dockerPorts.tdarr;
+      environment = {
+      	PUID = "950";
+      	PGID = "950";
+      	UMASK_SET = "022";
+      	TZ = "America/Los_Angeles";
+      	serverIP = hostIP;
+      	serverPort = "${toString ports.tdarrServer}";
+      	webUIPort = "${toString ports.tdarrWeb}";
+      	internalNode = "false";
+      	inContainer = "true";
+      	nodeName = "MyInternalNode";
+      	NVIDIA_VISIBLE_DEVICES = "1";
+      	NVIDIA_DRIVER_CAPABILITIES = "all";
+      };
+      volumes = [
+        "${arrConfigDir}/tdarr/server:/app/server"
+        "${arrConfigDir}/tdarr/configs:/app/configs"
+        "${arrConfigDir}/tdarr/logs:/app/logs"
+        "${data4kDir}:/data-4k"
+        "${dataDir}:/data"
+        "${tdarrTranscodeDir}:/temp"
+      ];
+      extraOptions = [
+      	"--runtime=nvidia"
+      	"--gpus=\"device=1\""
+      	"--network=plex-stack"
+      ];
+    };
+
+    tdarrNode = {
+      image = "ghcr.io/haveagitgat/tdarr_node:latest";
+      environment = {
+      	PUID = "950";
+      	PGID = "950";
+      	UMASK_SET = "022";
+      	TZ = "America/Los_Angeles";
+      	serverIP = hostIP;
+      	serverPort = "${toString ports.tdarrServer}";
+      	inContainer = "true";
+      	max_old_space_size = "8152";
+      	maxOldSpaceSize = "8152";
+      	nodeName = "PrimaryNode";
+      	NVIDIA_VISIBLE_DEVICES = "1";
+      	NVIDIA_DRIVER_CAPABILITIES = "all";
+      };
+      volumes = [
+        "${arrConfigDir}/tdarr/server:/app/server"
+        "${arrConfigDir}/tdarr/configs:/app/configs"
+        "${arrConfigDir}/tdarr/logs:/app/logs"
+        "${data4kDir}:/data-4k"
+        "${dataDir}:/data"
+        "${tdarrTranscodeDir}:/temp"
+      ];
+      extraOptions = [
+      	"--runtime=nvidia"
+      	"--gpus=\"device=1\""
+      	"--network=plex-stack"
+      ];
+    };
+
+    bazarr = {
+	  image = "ghcr.io/hotio/bazarr";
+	  ports = dockerPorts.bazarr;
+	  environmentFiles = [ ./plexDefault.env ];
+	  environment = {
+	  	UMASK_SET = "022";
+	  };
+	  volumes = [
+	    "${arrConfigDir}/bazarr/config:/config"
+	    "${arrConfigDir}/bazarr/logs:/logs"
+	  	"${dataDir}/movies:/data/movies"
+	  	"${dataFallbackDir}/shows:/data/shows-fallback"
+	  	"${dataDir}/shows:/data/shows"
+	  ];
+      extraOptions = [ "--network=plex-stack" ];
+	};
+  };
+}
