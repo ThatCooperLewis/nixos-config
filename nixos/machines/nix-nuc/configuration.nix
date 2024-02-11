a955f1fa5e2f4f7ccdf9673f4f511ee4a5d2ea56
Cooper Lewis <thatcooperlewis@gmail.com>
2024-02-11 15:00:22 -0800

    Setup grafana kiosk mode

--- nixos/machines/nix-nuc/configuration.nix
+++ nixos/machines/nix-nuc/configuration.nix
@@ -18,6 +18,14 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Enable automatic login
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "cooper";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+  
   # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
   # If no user is logged in, the machine will power down after 20 minutes.
   systemd.targets.sleep.enable = false;
@@ -88,14 +96,6 @@
   users.groups.uptime.gid = constants.users.uptime;
   users.groups.palworld.gid = constants.users.palworld;
   users.groups.telegraf.gid = constants.users.telegraf;
-
-  # Enable automatic login
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "cooper";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
