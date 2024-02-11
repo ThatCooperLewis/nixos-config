7b9207aefc2d15337c65b579227441a1d451660f
Cooper Lewis <thatcooperlewis@gmail.com>
2024-02-10 16:44:34 -0800

    Swap out locale on nix-nuc for defaults

--- nixos/machines/nix-nuc/configuration.nix
+++ nixos/machines/nix-nuc/configuration.nix
@@ -1,19 +1,10 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, constants ... }:
 
 {
-  time.timeZone = "America/Los_Angeles";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+  ### Locale
+  time.timeZone = constants.systemDefaults.timeZone;
+  i18n.defaultLocale = constants.systemDefaults.defaultLocale;
+  i18n.extraLocaleSettings = constants.systemDefaults.extraLocaleSettings;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
