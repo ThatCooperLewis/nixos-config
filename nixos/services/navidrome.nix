778dd50cbe29a19fc8883da13387c1eec282ab33
Cooper Lewis <thatcooperlewis@gmail.com>
2024-07-22 20:03:24 -0700

    Add navidrome for rpi

---
+++ nixos/services/navidrome.nix
@@ -0,0 +1,24 @@
+{ pkgs, app, user, constants, ... }:
+
+{
+
+  config.users.users.navidrome = {
+    uid = constants.users.navidrome;
+    description = "Navidrome Music";
+    isNormalUser = false;
+    group = "navidrome";
+    extraGroups = [ "wheel" ];
+  };
+  config.users.groups.navidrome.gid = constants.users.navidrome;
+  
+  config.services.navidrome = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      Port = constants.ports.navidrome;
+      DataFolder = "${constants.docker.dirs.navidrome}/data";
+      MusicFolder = "${constants.docker.dirs.navidrome}/music";
+      CacheFolder = "${constants.docker.dirs.navidrome}/cache";
+    };
+  };
+}
