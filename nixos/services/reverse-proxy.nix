2edbf075f702ad38ff8437593dd6330c6a736a5c
Cooper Lewis <thatcooperlewis@gmail.com>
2024-12-22 10:27:42 -0800

    Clean up caddy config format

--- nixos/services/reverse-proxy.nix
+++ nixos/services/reverse-proxy.nix
@@ -6,71 +6,73 @@
 
   services.caddy = {
   	enable = true;
+    virtualHosts = {
 
-  	# NUC Mini PC
-  	virtualHosts."http://sonarr.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.sonarr}
-  	'';
-  	virtualHosts."http://sonarr4k.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.sonarr4k}
-  	'';
-  	virtualHosts."http://radarr.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.radarr}
-    '';
-  	virtualHosts."http://radarr4k.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.radarr4k}
-    '';
-  	virtualHosts."http://prowlarr.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.prowlarr}
-    '';
-  	virtualHosts."http://overseerr.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.overseerr}
-    '';
-  	virtualHosts."http://requestrr.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.requestrr}
-    '';
-  	virtualHosts."http://tdarr.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.tdarr}
-    '';
-  	virtualHosts."http://tautulli.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.tautulli}
-    '';    
+      # NUC Mini PC
+      "http://sonarr.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.sonarr}
+      '';
+      "http://sonarr4k.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.sonarr4k}
+      '';
+      "http://radarr.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.radarr}
+      '';
+      "http://radarr4k.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.radarr4k}
+      '';
+      "http://prowlarr.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.prowlarr}
+      '';
+      "http://overseerr.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.overseerr}
+      '';
+      "http://requestrr.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.requestrr}
+      '';
+      "http://tdarr.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.tdarr}
+      '';
+      "http://tautulli.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.tautulli}
+      '';    
+      
+      # TrueNAS Apps
+      "http://nas.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.nas}
+      '';
+      "http://plex.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.plex}
+      '';
+      "http://sab.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.sab}
+      '';
+      "http://scrutiny.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.scrutiny}
+      '';
 
-    # TrueNAS Apps
-  	virtualHosts."http://nas.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.nas}
-    '';
-  	virtualHosts."http://plex.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.plex}
-    '';
-  	virtualHosts."http://sab.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.sab}
-    '';
-    virtualHosts."http://scrutiny.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.scrutiny}
-    '';
+      # Monitor Pi
+      "http://uptime.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.uptime}
+      '';
+      "http://grafana.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.grafana}
+      '';
 
-    # Monitor Pi
-  	virtualHosts."http://uptime.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.uptime}
-    '';
-  	virtualHosts."http://grafana.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.grafana}
-    '';
+      # Home Assistant
+      "http://ha.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.ha}
+      '';
 
-    # Home Assistant
-  	virtualHosts."http://ha.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.ha}
-    '';
+      # Wifi AP
+      "http://wifi.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.wifi}
+      '';
 
-    # Wifi AP
-  	virtualHosts."http://wifi.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.wifi}
-    '';
-
-    # Octoprint RasPi
-  	virtualHosts."http://octopi.lewis.arpa".extraConfig = ''
-      reverse_proxy ${constants.urls.octopi}
-    '';        
+      # Octoprint RasPi
+      "http://octopi.lewis.arpa".extraConfig = ''
+        reverse_proxy ${constants.urls.octopi}
+      '';        
+    };
   };
 }
