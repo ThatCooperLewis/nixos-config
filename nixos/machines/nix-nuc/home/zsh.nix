0461e5c71a143bc6586c7aff32019b3d05aea6b3
Cooper Lewis <thatcooperlewis@gmail.com>
2025-01-12 02:09:47 -0800

    Update pi machines with root SSH pub key

--- nixos/machines/nix-nuc/home/zsh.nix
+++ nixos/machines/nix-nuc/home/zsh.nix
@@ -9,7 +9,7 @@
       update = "sudo nixos-rebuild";
       update-home = "home-manager init --switch";
       update-caddy = "update switch --flake ~/Nix/nixos/#caddy-pi --target-host root@10.0.50.30 --verbose --fast";
-      update-cf-fallback = "update switch --flake ~/Nix/nixos/#cloudflare-fallback-pi --target-host root@10.0.50.31 --verbose --fast";
+      update-caddy-fallback = "update switch --flake ~/Nix/nixos/#cloudflare-fallback-pi --target-host root@10.0.50.31 --verbose --fast";
       update-fortress = "update switch --flake ~/Nix/nixos/#fortress-pi --target-host root@10.0.50.33 --verbose --fast";
 
       idport = "sudo netstat -tulpn | grep";
