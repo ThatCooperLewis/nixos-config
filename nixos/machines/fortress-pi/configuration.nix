d20a8184bfea74ab97061dfa40d162679671c6ac
Cooper Lewis <thatcooperlewis@gmail.com>
2025-01-12 22:41:30 -0800

    Create separate root SSH key config

--- nixos/machines/fortress-pi/configuration.nix
+++ nixos/machines/fortress-pi/configuration.nix
@@ -58,11 +58,6 @@ in {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
       };
-
-      root.openssh.authorizedKeys.keys = [
-        # NUC Root
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMr71CE+bzLkDKdvL7iBU/gETtgMNOK449EQl9JcDokd nix-nuc-root"
-      ];
     };
   };
 
