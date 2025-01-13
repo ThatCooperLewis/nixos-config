aed63b887607d74310dff04820ac67467b292aeb
Cooper Lewis <thatcooperlewis@gmail.com>
2025-01-12 22:27:40 -0800

    Move unused files to new archive dir

---
+++ archive/ARCHIVE.md
@@ -0,0 +1,3 @@
+### Nix Archive
+
+Configurations, services, containers, etc. that are unused, but still worthwhile to keep around for reference and revival.
---
+++ archive/flake.nix
@@ -0,0 +1,36 @@
+## Old primary linux machine, living but unused
+
+      "lewis-linux" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        # Set all input parameters as specialArgs of all sub-modules
+        # so that we can use the `helix`(an attribute in inputs) in
+        # sub-modules directly.
+        specialArgs = { inherit inputs constants; };
+        modules = [
+          # Hardware config
+          ./machines/lewis-linux/hardware-configuration.nix
+          # Primary configuration
+          ./machines/lewis-linux/configuration.nix
+          # KVM, QEMU, and other virtualization configs
+          ./virtualization.nix
+          # All docker containers
+          ./containers/containers.nix
+          # Telegraf metrics
+          ./services/telegraf.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/start-using-home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.cooper = import ./machines/lewis-linux/home/home.nix;
+          }
+
+          vscode-server.nixosModules.default
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
+        ];
+      };
