321f64519b49bf02009a95bd481ebdbe8c67ede7
Cooper Lewis <thatcooperlewis@gmail.com>
2025-01-15 20:30:10 -0800

    Add brain to root SSH

--- nixos/users/root/ssh.nix
+++ nixos/users/root/ssh.nix
@@ -2,5 +2,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     # NUC Root
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMr71CE+bzLkDKdvL7iBU/gETtgMNOK449EQl9JcDokd nix-nuc-root"
+
+    # Brain Root (/root/.ssh/id_ed25519.pub)
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMuFaNqrc/rm3EHHa1ah0I/S+wxXEC9vnmkse/kfNG0H nix-brain"
   ];
 }
