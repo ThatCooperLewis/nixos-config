e9bd65ada961ab2bbba7a2ad4035fbf1d6c1ef10
Cooper Lewis <thatcooperlewis@gmail.com>
2025-01-17 00:48:11 -0800

    Fix path for plex mirror backupDestination

--- nixos/backups/mirror-plex-data.nix
+++ nixos/backups/mirror-plex-data.nix
@@ -7,7 +7,7 @@
     (import ./rsync-base.nix {
       rsyncPackage      = pkgs.rsync;
       backupSource      = "${constants.nfs.dirs.plex.mountPath}";
-      backupDestination = "${constants.brain.dirs.plexMirror}";
+      backupDestination = "${constants.services.dirs.plexMirror}";
       serviceName       = "plexDataMirroring";
       schedule          = "*:0/10";
     })
