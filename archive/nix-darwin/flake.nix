5442c6190eecf0fbb5021ec3abe7d06ee415c019
Cooper Lewis <thatcooperlewis@gmail.com>
2025-01-24 16:58:35 -0800

    Add default nix-darwin config to archive

---
+++ archive/nix-darwin/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs-darwin }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [ 
+        micro
+        home-manager
+      ];
+
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."cooper-mbp" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
