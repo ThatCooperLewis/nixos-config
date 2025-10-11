65b6483921d4ded4674183b15b21afb030235718
Cooper Lewis <thatcooperlewis@gmail.com>
2025-10-11 16:05:53 -0700

    remove redundant dhcp setting

--- nixos/machines/nix-nas/hardware-configuration.nix
+++ nixos/machines/nix-nas/hardware-configuration.nix
@@ -28,12 +28,5 @@
     [ { device = "/dev/disk/by-uuid/3eacf5be-36ae-437c-827d-b45d21dc0b3b"; }
     ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.ens3.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
